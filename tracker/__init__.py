611a685a7263fc4198e3234e0f7d70efc3cec0f5
Joshua Halls <joshuah40@googlemail.com>
2020-11-13 23:21:33 +0000

    deployed and working on gcp

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -28,6 +28,10 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
+    @app.route("/")
+    def index():
+        return ("working")
+
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
